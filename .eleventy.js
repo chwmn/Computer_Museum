676f86a009fd515d04af03facbd572aead7a3205
chwmn <chulminh99@gmail.com>
2021-06-14 13:03:25 -0500

    basic configuration and sass installed

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+// docs: https://www.11ty.io/docs/config/
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.setTemplateFormats([
+    "css",
+    "html",
+    "liquid",
+    "jpg",
+    "png",
+    "svg",
+    "js",
+    "otf",
+    "woff",
+    "woff2",
+  ]);
+
+  function sortByPageOrder(values) {
+    return values.slice().sort((a, b) => a.data.order - b.data.order);
+  }
+
+  eleventyConfig.addFilter("sortByPageOrder", sortByPageOrder);
+
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "_layouts",
+      input: "src",
+      output: "docs",
+    },
+  };
+};
